d6a4dd1ad157cbfb03a843979130c8166a395ab4
ddomizia <domizia.derasmo@gmail.com>
2026-04-08 14:56:28 +0200

    risoluzione build

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,18 +1,11 @@
 import type {StructureResolver} from 'sanity/structure'
-import {CalendarDays, PawPrint} from 'lucide-react'
 
 export const structure: StructureResolver = (S) =>
   S.list()
     .title('Contenuti')
     .items([
-      S.documentTypeListItem('animal')
-        .title('Animali')
-        .icon(PawPrint),
-
-      S.documentTypeListItem('event')
-        .title('Eventi')
-        .icon(CalendarDays),
-
+      S.documentTypeListItem('animal').title('Animali'),
+      S.documentTypeListItem('event').title('Eventi'),
       ...S.documentTypeListItems().filter(
         (item) => !['animal', 'event'].includes(item.getId() || '')
       ),
